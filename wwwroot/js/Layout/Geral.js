dcae4fe3c374cb9934e36478bc6f389400669197
Victor-Eduardo <65134934+Victor-Eduardo@users.noreply.github.com>
2020-05-16 22:49:45 -0300

    Alterado menu mobile do header.

---
+++ wwwroot/js/Layout/Geral.js
@@ -0,0 +1,19 @@
+﻿if (screen.width <= 480) {
+    var icon = document.querySelector('.icon');
+    icon.addEventListener('click', function () {
+        var myLinks = document.querySelector('#myLinks');
+
+        if (myLinks.style.display === "block") {
+            myLinks.style.display = "none";
+        } else {
+            myLinks.style.display = "block";
+        }
+    });
+}
+
+else {
+    var topMobile = document.querySelector('.top-mobile-nav');
+    topMobile.style.display = "none";
+}
+
+
