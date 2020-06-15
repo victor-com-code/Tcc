10f1779064c60e5d2b9feb50780739042c35ff59
thiagocmg <65470706+thiagocmg@users.noreply.github.com>
2020-06-15 19:21:11 -0300

    Modificações de forms nos layouts

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -16,23 +16,19 @@ namespace Tcc_Senai.Models
         [MaxLength(30)]
         [Display(Name = "Nome Completo")]
         public string NomeCompleto { get; set; }
-        public string PrimeiroNome { get; set; }
 
         [Required(ErrorMessage = "O campo Contrato é obrigatório.")]
         [Display(Name = "Contrato")]
         public string TipoDeContrato { get; set; }
 
-        [Required(ErrorMessage = "O campo Horário de Trabalho é obrigatório.")]
         [Display(Name = "Horário de Trabalho")]
         [Range(1, 10, ErrorMessage = "A jornada de trabalho diária não pode ultrapassar 10 horas.")]
         public int HorarioDeTrabalho { get; set; }
 
-        [Required(ErrorMessage = "O campo Carga Horária Semanal é obrigatório.")]
         [Display(Name = "Carga Horária Semanal")]
         [Range(1, 70, ErrorMessage = "A jornada de trabalho semanal não pode ultrapassar 70 horas.")]
         public int CargaHorariaSemanal { get; set; }
 
         public virtual ICollection<ProfessorCurso> ProfessorCursos { get; set; }
-
     }
 }
