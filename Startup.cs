1fed653850f7220185879f8f7a7fd18a71d73305
Victor-Eduardo <65134934+Victor-Eduardo@users.noreply.github.com>
2020-07-08 11:42:12 -0300

    Last Commit

--- Startup.cs
+++ Startup.cs
@@ -49,7 +49,7 @@ namespace Tcc_Senai
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Funcionario}/{action=Login}/{id?}");
             });
         }
     }
